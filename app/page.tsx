23841e3d71fd5432d464f59cd886772808d7e403
Nethasha Fernando <nethashaf@trafforge.com>
2026-06-22 22:42:53 +0530

    message

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import {events} from "@/lib/constants";
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
 
+
  // const Page = async ()=>{
  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
  const Page = async ()=>{
      'use cache'
      cacheLife('hours')
-   const response=await fetch(`${BASE_URL}/api/events`);
-   const {events}=await response.json(); //destructure and get only events
+   // const response=await fetch(`${BASE_URL}/api/events`);
+   // const {events}=await response.json(); //destructure and get only events
    return(
       <section>
         <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't Miss</h1>
