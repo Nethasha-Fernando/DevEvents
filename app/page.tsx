1da8d724c75fdcade829b1aa32cbc5d021d1a760
NethashaFernando <nethashaf123@gmail.com>
2026-06-23 10:55:39 +0530

    removed api calls

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,16 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import {events} from "@/lib/constants";
 import {IEvent} from "@/database";
-import {cacheLife} from "next/cache";
+import {cacheLife} from "next/cache"
+
 
  // const Page = async ()=>{
- const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+ // const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
  const Page = async ()=>{
-     'use cache'
-     cacheLife('hours')
-    const response=await fetch(`${BASE_URL}/api/events`);
-    const {events}=await response.json(); //destructure and get only events
+    //  'use cache'
+    //  cacheLife('hours')
+    // const response=await fetch(`${BASE_URL}/api/events`);
+    // const {events}=await response.json(); //destructure and get only events
    return(
       <section>
         <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't Miss</h1>
@@ -21,11 +22,11 @@ import {cacheLife} from "next/cache";
           <div id="events" className="mt-20 space-y-7">
               <h3>Featured Events</h3>
               <ul className="events">
-                  {events?.map((event: IEvent) => (
-                      <li key={event.title} className="list-none">
-                          <EventCard {...event} />
-                      </li>
-                  ))}
+                  {/*{events?.map((event: IEvent) => (*/}
+                  {/*    <li key={event.title} className="list-none">*/}
+                  {/*        <EventCard {...event} />*/}
+                  {/*    </li>*/}
+                  {/*))}*/}
               </ul>
 
           </div>
